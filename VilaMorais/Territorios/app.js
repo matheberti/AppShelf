2343f72d65c1eeb4c8ecefe9316cbc7c49fffe4b
Quarterolli <58645471+Quarterolli@users.noreply.github.com>
2024-12-26 20:38:59 +0000

    edit `app.js` with better names and using events `click` and `contextmenu` to handle map interactions as well as make minor edit on `index` and `style`

--- VilaMorais/Territorios/app.js
+++ VilaMorais/Territorios/app.js
@@ -1,98 +1,144 @@
 (async () => {
   "use strict"
 
-  const coveredBlocks = new Set,
-  i = document.querySelector("main"),
-  handlePointerEvent = e => {
-    if (e.target?.parentElement?.id !== "g" + t.dataset.n || t.dataset.n === "0" || window.visualViewport.scale > 1) return
+  const view = {
+    title: document.getElementById("title"),
+    compass: document.getElementById("compass"),
+    alert: document.getElementById("alert"),
+    message: document.getElementById("message"),
+    button: document.getElementById("button"),
+    menu: document.getElementById("menu"),
+    homeItem: document.getElementById("i0"),
+    map: document.getElementById("map"),
+    group: document.getElementById("group"),
+    numbers: document.getElementById("numbers"),
+    locate: document.getElementById("locate"),
+    notify: document.getElementById("notify")
+  }
+  const coveredBlocks = new Set
+  const warnUser = note => {
+    view.message.textContent = note
+    view.alert.showModal()
+  }
+  const handleMapTouches = touch => {
+    touch.preventDefault()
 
-    let target = e.target
-    while (target.tagName !== "path") target = target.previousElementSibling
+    const territoryNumber = view.title.dataset.number
+    let target = touch.target
+
+    if (territoryNumber === "0" || target.parentElement.id !== "g" + territoryNumber) {
+      return
+    }
+
+    while (target.tagName !== "path") {
+      target = target.previousElementSibling
+    }
 
     let label = target.nextElementSibling.textContent
-    if (target.nextElementSibling?.nextElementSibling?.tagName === "text") label += "/" + target.nextElementSibling.nextElementSibling.textContent
-    
-    if (e.type == "pointerup" && coveredBlocks.size === 0) {
+    if (target.nextElementSibling?.nextElementSibling?.tagName === "text") {
+      label += "/" + target.nextElementSibling.nextElementSibling.textContent
+    }
+
+    if (touch.type === "click") {
+      const notFilled = !target.hasAttribute("fill")
+
+      if (notFilled) {
+        target.setAttribute("fill", "#333")
+        coveredBlocks.add(label)
+      } else {
+        target.removeAttribute("fill")
+        coveredBlocks.delete(label)
+      }
+    }
+
+    if (touch.type === "contextmenu" && coveredBlocks.size === 0) {
       const [latitude, longitude] = parameters[target.parentElement.id.replace("g", "i")][label]
+      
+      navigator.vibrate(200)
       window.location.href = `https://www.google.com/maps/dir/?api=1&travelmode=driving&destination=${latitude},${longitude}`
     }
-    
-    const hasAtt = target.hasAttribute("fill")
-
-    if (e.type == "pointerup" && hasAtt) {
-      remAtt(target, "fill")
-      coveredBlocks.delete(label)
-    }
-
-    if (e.type == "pointercancel" && !hasAtt) {
-      setAtt(target, "fill", "#333")
-      coveredBlocks.add(label)
-    }
-  },
-  forAll = (s, f) => document.querySelectorAll(s).forEach(f),
-  listen = (l, t, f) => l.addEventListener(t, f),
-  setAtt = (l, a, v) => a !== "t" ? l.setAttribute(a, v) : l.setAttribute("transform", `rotate(${v})`),
-  remAtt = (l, a) => l.removeAttribute(a),
-  modal = t => { m.textContent = t, a.showModal() },
-  toggleU = () => u.classList.toggle("s"),
-  clearBlocks = () => coveredBlocks.clear()
+  }
 
   let parameters
   try {
     parameters = await (await fetch("param.json")).json()
   } catch {
-    modal("Não foi possível ativar a interação do mapa, verifique sua internet.")
+    warnUser("Não foi possível ativar a interação do mapa, verifique sua internet.")
   }
 
-  listen(i, "pointerup", handlePointerEvent)
-  listen(i, "pointercancel", handlePointerEvent)
-  listen(a, "click", () => a.close())
-  forAll("li", l => listen(l, "click", e => {
-    const { degrees, viewBox } = parameters[e.target.id],
-    hideAndShow = (h, s) => { h.classList.add("h"), s.classList.remove("h") }
+  view.button.addEventListener("click", () => view.alert.close())
 
-    t.textContent = e.target.textContent
-    t.dataset.n = e.target.id.slice(1)
+  document.querySelectorAll("li").forEach(item => item.addEventListener("click", click => {
+    const item = click.target
+    const { degrees, viewBox } = parameters[item.id]
+    const territoryNumber = item.id.slice(1)
 
-    if (e.target.id === "i0") {
-      hideAndShow(i0, b)
+    view.title.textContent = item.textContent
+    view.title.dataset.number = territoryNumber
+
+    if(territoryNumber !== "0"){
+      document.querySelectorAll(`#g${territoryNumber} path`).forEach(block =>
+        block.setAttribute("style", "fill-opacity:1")
+      )
+      view.homeItem.classList.remove("hide")
+      view.numbers.classList.add("hide")
     } else {
-      hideAndShow(b, i0)
-      forAll(`#g${t.dataset.n} path`, p => setAtt(p, "style", "fill-opacity:1"))
+      view.homeItem.classList.add("hide")
+      view.numbers.classList.remove("hide")
     }
-    setAtt(p, "viewBox", viewBox)
-    setAtt(g, "t", degrees)
-    setAtt(c, "t", degrees)
-    toggleU()
-  }))
-  listen(f, "click", () => {
-    toggleU()
-    clearBlocks()
 
-    if (t.dataset.n !== "0") {
-      forAll(`#g${t.dataset.n} path`, p => { remAtt(p, "fill"), remAtt(p, "style") })
+    view.map.setAttribute("viewBox", viewBox)
+    view.group.setAttribute("transform", `rotate(${degrees})`)
+    view.compass.setAttribute("transform", `rotate(${degrees})`)
+    view.menu.classList.remove("show")
+  }))
+
+  view.map.addEventListener("click", handleMapTouches)
+  view.map.addEventListener("contextmenu", handleMapTouches)
+
+  view.locate.addEventListener("click", () => {
+    const territoryNumber = view.title.dataset.number
+
+    view.menu.classList.add("show")
+
+    coveredBlocks.clear()
+
+    if (territoryNumber !== "0") {
+      document.querySelectorAll(`#g${territoryNumber} path`).forEach(block => {
+        block.removeAttribute("fill")
+        block.removeAttribute("style")
+      })
     }
   })
-  listen(n, "click", async () => {
+
+  view.notify.addEventListener("click", async () => {
     if (coveredBlocks.size === 0) {
-      modal("Nenhum território selecionado para informar.")
+      warnUser("Nenhum território selecionado para informar.")
       return
     }
 
-    const filled = [...coveredBlocks].sort().join(", ").replaceAll(",", (match, index, string) => index === string.lastIndexOf(match) ? " e" : match)
-    const data = { text: `Ministério ${new Date().toLocaleDateString("pt-BR")}\nTerritório: ${t.dataset.n}\nQuadras: ${filled}\nObservações: ` }
+    const today = new Date().toLocaleDateString("pt-BR")
+    const territoryNumber = view.title.dataset.number
+    const blocksCoordination = [...coveredBlocks].sort().reduce((coordination, block, index, array) =>
+      index !== array.length - 1
+        ? coordination + ", " + block
+        : coordination + " e " + block
+    )
+    const ministryReportToday = {
+      text: `Ministério ${today}\nTerritório: ${territoryNumber}\nQuadras: ${blocksCoordination}\nObservações: `
+    }
 
-    clearBlocks()
-    forAll(`#g${t.dataset.n} path`, p => remAtt(p, "fill"))
+    coveredBlocks.clear()
+    document.querySelectorAll(`#g${territoryNumber} path`).forEach(block => block.removeAttribute("fill"))
 
     try {
-      await navigator.share(data)
+      await navigator.share(ministryReportToday)
     } catch {
       try {
-        await navigator.clipboard.writeText(data.text)
-        modal("Copiado para a área de transferência.")
+        await navigator.clipboard.writeText(ministryReportToday.text)
+        warnUser("Copiado para a área de transferência.")
       } catch {
-        modal("Este dispositivo não permite compartilhar informações com outros aplicativos.")
+        warnUser("Este dispositivo não permite compartilhar informações com outros aplicativos.")
       }
     }
   })
