f0345c1c1a70f5d20ae3ba46f8fa14c4118eac85
Quarterolli <58645471+Quarterolli@users.noreply.github.com>
2025-02-03 01:21:34 +0000

    Adds do-not-call notes functionality to the test environment

--- VilaMorais/Territorios/app.js
+++ VilaMorais/Territorios/app.js
@@ -7,6 +7,7 @@
     alert: document.getElementById("alert"),
     message: document.getElementById("message"),
     button: document.getElementById("button"),
+    notations: document.getElementById("notations"),
     menu: document.getElementById("menu"),
     homeItem: document.getElementById("i0"),
     map: document.getElementById("map"),
@@ -57,10 +58,10 @@
       if (coveredBlocks.size !== 0) {
         warnUser("Limpe as quadras selecionadas antes de entrar no aplicativo de navegação.")
       } else {
-        try{
+        try {
           const [latitude, longitude] = parameters[target.parentElement.id.replace("g", "i")][label]
           const formattedLongitude = longitude < 100 ? `0${longitude}` : longitude
-          
+
           navigator.vibrate(200)
           window.location.href = `https://www.google.com/maps/dir/?api=1&travelmode=driving&destination=-16.6${latitude},-49.2${formattedLongitude}`
         } catch {
@@ -79,6 +80,10 @@
 
   view.button.addEventListener("click", () => view.alert.close())
 
+  view.notations.addEventListener("click", () => {
+    warnUser("Este território possui endereços que não devem ser visitados. Contate o dirigente de campo ou o servo de territórios.")
+  })
+
   document.querySelectorAll("li").forEach(item => item.addEventListener("click", click => {
     const item = click.target
     const { degrees, viewBox } = parameters[item.id]
@@ -87,7 +92,7 @@
     view.title.textContent = item.textContent
     view.title.dataset.number = territoryNumber
 
-    if(territoryNumber !== "0"){
+    if (territoryNumber !== "0") {
       document.querySelectorAll(`#g${territoryNumber} path`).forEach(block =>
         block.setAttribute("style", "fill-opacity:1")
       )
@@ -102,6 +107,12 @@
     view.group.setAttribute("transform", `rotate(${degrees})`)
     view.compass.setAttribute("transform", `rotate(${degrees})`)
     view.menu.classList.remove("show")
+    view.notations.classList.add("hide")
+    
+    if (territoryNumber === "18") {
+      warnUser("Este território possui endereços que não devem ser visitados. Contate o dirigente de campo ou o servo de territórios.")
+      view.notations.classList.remove("hide")
+    }
   }))
 
   view.map.addEventListener("click", handleMapTouches)
